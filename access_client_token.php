6ae7f347903c643115737147aadb1f5605f035d9
Silvia Sirbu <ssilvias15@yahoo.com>
2022-08-22 09:16:22 +0100

    Update access_client_token.php

--- access_client_token.php
+++ access_client_token.php
@@ -3,10 +3,8 @@
 $curl = curl_init();
 
 //API CREDENTIALS
-$clientId = "ARSqHC295J546ZD6NANrJMi7ZS7YCJhQaQc1qDBwwKY1Aqxkm0YuiSxvBGHzZH4iWMPNynrAu0SeKw-o";
-//$clientId = "AVBHmsgNyCn_dsjYcFoQr5eFY8a1MChnCDtkf3xSBg4sbKo5yyHRhp5YeQJWAuoL6nuuzOwqzuWFg8t7";
-$secret = "ELenJV_oi9daE2HEZc3HgjO5grCa3DfiUxYuZNg7OSHbOKFU0uncjckPVxzCEThTB-sKqrAgeNbTm86E";
-//$secret = "EAtLQSnY9VvB1NQJlXByJLtQqNvPAmPd7ysnK2NxcnNsSylMhnE0K7ooXEQxgewHYaR98YLM-ICDpVfG";
+$clientId = "xxxxxxxxxxxxxxx";
+$secret = "xxxxxxxxxxxxxx";
 
 curl_setopt_array($curl, array(
     CURLOPT_URL => "https://api.sandbox.paypal.com/v1/oauth2/token",
